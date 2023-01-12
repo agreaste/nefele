651aebf904e7bb694d3a4026e62c559237a9f416
Andrea Storace <a.storace@bitmama.it>
2023-01-12 09:24:56 +0100

    updated configurations

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,13 +1,13 @@
-module.exports =  ({ env }) => ({
-	connection: {
-		client: 'mysql',
-		connection: {
-		host: env('DATABASE_HOST', '0.0.0.0'),
-			port: env.int('DATABASE_PORT', 3306),
-			database: env('DATABASE_NAME', 'bit-db'),
-			user: env('DATABASE_USERNAME', 'andreo'),
-			password: env('DATABASE_PASSWORD', 'andreo123'),
-			ssl: env.bool('DATABASE_SSL', false)
-		}
-	}
+module.exports = ({env}) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('PROD', false) ? `/cloudsql/${env('DB_INSTANCE_NAME')}` : env('DB_INSTANCE_NAME', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'bit-db'),
+      user: env('DATABASE_USERNAME', 'andreo'),
+      password: env('DATABASE_PASSWORD', 'andreo123'),
+      ssl: env.bool('DATABASE_SSL', false)
+    }
+  }
 });
